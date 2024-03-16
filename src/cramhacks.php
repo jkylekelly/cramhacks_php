0c7f836ac81a263ef149e88e5882283d6d72b749
jkylekelly <securityresearch@r2c.dev>
2024-03-16 16:46:15 -0700

    initial commit

---
+++ src/cramhacks.php
@@ -0,0 +1,19 @@
+<?php
+// File: CramhacksAscii.php
+
+namespace CramhacksAscii;
+
+class CramhacksAscii {
+    public static function printAscii() {
+        echo "
+                             _                _        
+                            | |              | |       
+     ___ _ __ __ _ _ __ ___ | |__   __ _  ___| | _____ 
+    / __| '__/ _` | '_ ` _ \\| '_ \\ / _` |/ __| |/ / __|
+    | (__| | | (_| | | | | | | | | | (_| | (__|   <__ \\
+     \\___|_|  \\__,_|_| |_| |_|_| |_|\\__,_|\\___|_|\\_\\___/
+                                                         
+                                                         
+        ";
+    }
+}
